b214338d5a877bdb0111b42b1d1356e44d79ad88
Luciano Brito <lucianobrito.dev@gmail.com>
2021-07-27 20:56:29 -0300

    Added WagonTest

---
+++ src/test/java/desafio/test/WagonTest.java
@@ -0,0 +1,31 @@
+package desafio.test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.junit.Test;
+
+import desafio.entities.Wagon;
+import desafio.enuns.WayWagonEnum;
+
+public class WagonTest {
+	
+	//deve passar
+	@Test
+	public void testGetOrder() {
+		Wagon wg = new Wagon(1, WayWagonEnum.LEFT);
+		wg.setOrder(10);
+		
+		assertThat(wg.getOrder(), is(10));
+	}
+	
+	//deve passar
+	@Test
+	public void testGetWay() {
+		Wagon wg = new Wagon(1, WayWagonEnum.LEFT);
+		wg.setOrder(10);
+		
+		assertThat(wg.getWay(), is(WayWagonEnum.LEFT.name()));
+	}
+
+}
