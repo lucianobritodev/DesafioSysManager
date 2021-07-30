ce916ad810e50f99c7425d4dab3fd3a961d604e8
Luciano Brito <lucianobrito.dev@gmail.com>
2021-07-29 21:27:34 -0300

    Update Count.java

--- src/main/java/desafio/util/Count.java
+++ src/main/java/desafio/util/Count.java
@@ -5,28 +5,18 @@ import java.util.Arrays;
 public class Count {
 		
 	public static int countNumbers(int[] arr, int lessThan) {
-		int count = 0;
 		int[] arrSort = sort(arr);
-		for (int num : arrSort) {
-			if (num < lessThan) {
-				count++;
-			}
-		}
-		System.out.println("Total de indices menores que " + lessThan + ": " + count);
+		System.out.print("\nArray ordenado: " + Arrays.toString(arrSort));
+		
+		int count = (int) Arrays.stream(arr).filter(num -> num < lessThan).count();
+		
+		System.out.print("\nTotal de indices menores que " + lessThan + ": " + count);
 		return count;	
 	}
 	
 	public static int[] sort(int[] arr) {
 		Arrays.sort(arr);
-		System.out.print("Array ordenado: ");
-		printArray(arr);
 		return arr;
 	}
 	
-	public static void printArray(int[] arr) {
-		for (int num : arr) {
-			System.out.print(num + ", ");
-		}
-		System.out.println();
-	}
 }
